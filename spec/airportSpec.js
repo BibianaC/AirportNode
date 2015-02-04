e6d286f71df83eb1e062291074149f85904ffe66
bibiana <bcristofol@gmail.com>
2015-02-04 10:28:20 +0000

    Airport should accept a plane

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,8 +1,10 @@
 var Airport = require('../src/airport');
+var Plane = require('../src/plane');
 
 describe('airport',function(){
 
   var airport = new Airport();
+  var plane = new Plane();
 
   it('should have zero planes initially',function(){
     expect(airport.planes).toBe(0);
@@ -10,6 +12,11 @@ describe('airport',function(){
 
   it('should have a default capacity of 20', function() {
     expect(airport.capacity).toBe(20);
+  });
+
+  it('should be able to accept planes', function() {
+    airport.accept(plane);
+    expect(airport.planes).toBe(1);
   });
 
 });
--- src/airport.js
+++ src/airport.js
@@ -1,7 +1,16 @@
 function Airport(){
    this.planes = 0;
    this.capacity = 20;
+   this.runaway = [];
 }
+
+Airport.prototype.accept = function(plane) {
+  if (this.capacity - 1 > this.planes) {
+    this.runaway.push(plane);
+    plane.land();
+    this.planes += 1;
+  }
+};
 
 
 module.exports = Airport;
